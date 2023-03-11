a17f4f80282786711c33c1dd68bbfad7c79abd3b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-03-11 18:09:55 +0530

    Update script.js

--- embed/js/script.js
+++ embed/js/script.js
@@ -183,7 +183,7 @@ for (let i = 0; i < allMusic.length; i++) {
                   <p>${allMusic[i].artist}</p>
                 </div>
                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
-                <audio preload="auto" class="${allMusic[i].src}" src="https://samir-paul.github.io/audio/${allMusic[i].src}.mp3"></audio>
+                <audio class="${allMusic[i].src}" src="https://samir-paul.github.io/audio/${allMusic[i].src}.mp3"></audio>
               </li>`;
   ulTag.insertAdjacentHTML("beforeend", liTag); //inserting the li inside ul tag
 
