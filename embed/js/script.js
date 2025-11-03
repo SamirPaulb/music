40293c5a30c4cf266da0a7c8f98763d7ecc14886
Samir Paul <77569653+SamirPaulb@users.noreply.github.com>
2025-11-04 01:29:51 +0530

    Update audio source URLs to new domain

--- embed/js/script.js
+++ embed/js/script.js
@@ -24,7 +24,7 @@ function loadMusic(indexNumb){
   musicName.innerText = allMusic[indexNumb - 1].name;
   musicArtist.innerText = allMusic[indexNumb - 1].artist;
   musicImg.src = `#`;
-  mainAudio.src = `https://spcdn.pages.dev/music/${allMusic[indexNumb - 1].src}.mp3`;
+  mainAudio.src = `https://scdn.vercel.app/music/${allMusic[indexNumb - 1].src}.mp3`;
 }
 
 //play music function
@@ -183,7 +183,7 @@ for (let i = 0; i < allMusic.length; i++) {
                   <p>${allMusic[i].artist}</p>
                 </div>
                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
-                <audio preload="auto" class="${allMusic[i].src}" src="https://spcdn.pages.dev/music/${allMusic[i].src}.mp3"></audio>
+                <audio preload="auto" class="${allMusic[i].src}" src="https://scdn.vercel.app/music/${allMusic[i].src}.mp3"></audio>
               </li>`;
   ulTag.insertAdjacentHTML("beforeend", liTag); //inserting the li inside ul tag
 
